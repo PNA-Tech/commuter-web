c8ee5056db86e1d4062b4eb8e3263050ac7ba3e5
Piyush Acharya <acharyapiyush1@outlook.com>
2023-10-29 15:43:15 -0700

    Update +page.svelte

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -8,7 +8,7 @@
 <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center mb-5">
     <div class="col">
         <div class="card">
-        <img src="/resources/carbon-footprint.jpg" class="card-img-top" alt="Carbon Footprint"/>
+        <img src="https://sancell.com.au/wp-content/uploads/2020/02/carbon-footprint-infographic2-scaled.jpg" class="card-img-top" alt="Carbon Footprint"/>
         <div class="card-body">
             <h5 class="card-title">Understanding Your Carbon Footprint</h5>
             <p class="card-text">Discover the basics of calculating and understanding your carbon footprint. Knowing your carbon footprint is the first step in reducing it. Learn how to measure and assess your impact on the environment.</p>
@@ -19,7 +19,7 @@
 
     <div class="col">
         <div a class="card">
-        <img src="/resources/carbon-reduction.jpg" class="card-img-top" alt="Carbon Reduction"/>
+        <img src="https://iste.web.unc.edu/wp-content/uploads/sites/21984/2021/02/carbon-footprint.jpg" class="card-img-top" alt="Carbon Reduction"/>
         <div class="card-body">
             <h5 class="card-title">Reducing Your Carbon Footprint</h5>
             <p class="card-text">Explore practical steps and lifestyle changes to reduce your carbon footprint. From energy-saving tips to sustainable transportation options, you can make a significant impact on the environment.</p>
@@ -30,7 +30,7 @@
 
     <div class="col">
         <div class="card">
-            <img src="/resources/eco-friendly-products.jpg" class="card-img-top" alt="Eco-Friendly Products"/>
+            <img src="https://th.bing.com/th/id/OIP.3HJVnVvY-jnebuMCTxR2OAHaE8?pid=ImgDet&rs=1" class="card-img-top" alt="Eco-Friendly Products"/>
             <div class="card-body">
             <h5 class="card-title">Eco-Friendly Products</h5>
             <p class="card-text">Discover a range of eco-friendly products that can help you reduce your carbon footprint. From energy-efficient appliances to sustainable consumer goods, find products that align with your environmental goals.</p>
@@ -41,7 +41,7 @@
 
     <div class="col">
         <div class="card">
-            <img src="/resources/climate-action.jpg" class="card-img-top" alt="Climate Action"/>
+            <img src="https://th.bing.com/th/id/R.d7a4afd034097bb2303b0002ed3df158?rik=tNdq3v04wFaUnQ&pid=ImgRaw&r=0" class="card-img-top" alt="Climate Action"/>
             <div class="card-body">
             <h5 class="card-title">Taking Climate Action</h5>
             <p class="card-text">Explore initiatives, organizations, and actions you can take to contribute to global climate solutions. Get involved in climate advocacy, support environmental organizations, and join the movement for a sustainable future.</p>
@@ -52,7 +52,7 @@
 
     <div class="col">
         <div class="card">
-            <img src="/resources/climate-education.jpg" class="card-img-top" alt="Climate Education"/>
+            <img src="https://th.bing.com/th/id/OIP.V7sPjIhQ_A4P6snnjA9ycAHaEA?pid=ImgDet&rs=1" class="card-img-top" alt="Climate Education"/>
             <div class="card-body">
             <h5 class="card-title">Climate Education</h5>
             <p class="card-text">Educate yourself about climate change, its effects, and the science behind it. Knowledge is a powerful tool in the fight against climate change. Explore resources for climate education and awareness.</p>
@@ -63,7 +63,7 @@
 
     <div class="col">
         <div class="card">
-            <img src="/resources/climate-action.jpg" class="card-img-top" alt="Climate Action"/>
+            <img src="https://th.bing.com/th/id/OIP.SFTtCBSGRuzwnXAX9JQ-OgHaEE?pid=ImgDet&rs=1" class="card-img-top" alt="Climate Action"/>
             <div class="card-body">
             <h5 class="card-title">Supporting Renewable Energy</h5>
             <p class="card-text">Learn about renewable energy sources and how you can support clean energy initiatives. Transitioning to renewable energy is a key step in reducing carbon emissions and combating climate change.</p>
